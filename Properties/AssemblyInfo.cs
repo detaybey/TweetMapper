09ed88c778c2e8a4a1fb8d52128bd794efd4e70d
Umut Çelenli <umut@celenli.com>
2015-04-12 01:36:07 +0300

    AssemblyInfo changed

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("twitconsole")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TweetMapper")]
+[assembly: AssemblyDescription("A tool for 140journos")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("twitconsole")]
+[assembly: AssemblyCompany("Umut Celenli")]
+[assembly: AssemblyProduct("TweetMapper")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
